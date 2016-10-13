1f206fe819d34a86cb641eeb6688c3766141419a
sayantan <4auddy@informatik-uni-hamburg.de>
2016-10-13 18:50:39 +0200

    updated directory structure

--- scripts/demo-client.py
+++
@@ -1,8 +0,0 @@
-import socket               
-
-s = socket.socket()        
-host = '192.168.0.102'# ip of raspberry pi 
-port = 12345               
-s.connect((host, port))
-print(s.recv(1024))
-s.close()
